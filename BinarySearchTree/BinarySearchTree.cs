ccf93670f6f2b6cc447a4ad91980185efe4ddbfe
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-01 15:07:31 -0500

    Added member variables 'data', 'leftNode', and 'rightNode'. These nodes will represent the individual nodes that make up binary search tree

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    class BinarySearchClass
+    class BinarySearchTree
     {
     }
 }
--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -8,5 +8,16 @@ namespace BinarySearchTree
 {
     class Node
     {
+        public int data;
+        public Node leftNode;
+        public Node rightNode;
+
+        public Node(int data)
+        {
+            this.data = data;
+            leftNode = null;
+            rightNode = null;
+        }
+
     }
 }
